e01b95de88cf2dceae457d393b7fd26155c112b6
Umochka <dirotz@mail.ru>
2023-05-10 08:29:34 +0300

    kjkl

---
+++ Zadacha_36/Zadacha_38/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+// [3 7 22 2 78] -> 76
+
+
+double[] arr = { 3, 7, 22, 2, 78 };
+
+double min = arr[0];
+double max = arr[0];
+
+for (int i = 1; i < arr.Length; i++)
+{
+    if (arr[i] < min)
+    {
+        min = arr[i];
+    }
+    if (arr[i] > max)
+    {
+        max = arr[i];
+    }
+}
+
+double diff = max - min;
+
+Console.WriteLine(diff);
